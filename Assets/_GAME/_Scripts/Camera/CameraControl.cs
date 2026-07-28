c31c062cbfb571f51a3197455ad1555197227b2f
ThienNgo02 <ngonamthien2002hd@gmail.com>
2026-07-28 22:37:26 +0700

    ~ update

---
+++ Assets/_GAME/_Scripts/Camera/CameraControl.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+namespace GameYT
+{
+    public class CameraControl : Singleton<CameraControl>
+    {
+        [SerializeField] Transform cam;
+
+        private void Update()
+        {
+
+        }
+    }
+}
